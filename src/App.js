02e9bb1f4ba7ac648f57588b9ac36aee8dd303dc
VanGuardf <alisaadati18000@gmail.com>
2024-04-06 09:04:43 +0330

    Add accordian component to render items

--- src/App.js
+++ src/App.js
@@ -34,3 +34,19 @@ function Accordion({ data }) {
   );
 }
 
+function AccordionItem({ text, title, num }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen(() => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      <div className="content-box">{isOpen && text}</div>
+    </div>
+  );
+}
